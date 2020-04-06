858a8cd7cec316f143b13be29b3c18b38c72b01a
Your Name <you@example.com>
2020-04-06 09:32:36 +0800

    Add -f to cp

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 wget http://gosspublic.alicdn.com/ossutil/1.6.10/ossutil64
 chmod 755 ossutil64
 ./ossutil64 config -e oss-cn-beijing.aliyuncs.com -i $ACCESSKEY -k $SECRETKEY
-./ossutil64 rm -rf oss://kan-bin/
-./ossutil64 cp ./kan oss://kan-bin/
+./ossutil64 cp -f ./kan oss://kan-bin/
